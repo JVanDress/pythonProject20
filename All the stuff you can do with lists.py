5ecb8ec3dce6502d3aa1b45bf0c173f6a4f7ad2e
JVanDress <jvandress@gmail.com>
2022-11-19 21:39:07 -0500

    committing first file to github

---
+++ All the stuff you can do with lists.py
@@ -0,0 +1,22 @@
+letters = ["a", "b", "c"]
+# Matrix is a two dimensional list
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+#Combining zero's + Letters is concacinating.
+#using the combined varible below this is our
+#concacination
+
+
+combined = zeros + letters
+numbers = list(range(20))
+# in a list you can do alot
+#like hello world down below
+#Josh, if you use chars = list it will change it to another
+#list man really cool.
+chars = list("hello World")
+print(len(chars))
+
+
+
+
+
