9fb3ba6129adde5ffa5751a51dea7785a74d9fbe
JVanDress <jvandress@gmail.com>
2022-11-19 21:09:46 -0500

    committing first file to github

---
+++ main double asterik.py
@@ -0,0 +1,11 @@
+def save_user(**user):
+ print(user)
+
+
+save_user(id=1, name="John", age=22)
+print=(save_user)
+
+
+
+
+
---
+++ multiple number of ways to pass arugments to display.py
@@ -0,0 +1,13 @@
+[2, 3, 4, 5]
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+       total *= number
+    return total
+
+print(multiply(2, 3, 4, 5))
+
+
+
+
