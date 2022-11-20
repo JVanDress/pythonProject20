4d740a1ac9d01cb1b02f45d68af98b08d497aeb4
JVanDress <jvandress@gmail.com>
2022-11-20 03:56:54 -0500

    committing first file to github

---
+++ make lists with tuples inside.py
@@ -0,0 +1,11 @@
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
+
+[(1, 10), (2, 20), (3, 30)]
+print(list(zip("abc", list1, list2)))
+
+
+#The way list files work is that you can make multple list's and combine them using the ZIP
+#all lists get matched up with the first number going down into the next list and that becomes
+#and or 
+
