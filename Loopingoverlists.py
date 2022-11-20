84595d7fb730909c830c9207ddeaec942941ac44
JVanDress <jvandress@gmail.com>
2022-11-20 00:52:06 -0500

    committing first file to github

---
+++ Loopingoverlists.py
@@ -0,0 +1,21 @@
+letters = ["a", "b", "c"]
+
+#unpacking list's tuples and the like it is
+#confusing be careful
+
+for index, letter in enumerate( letters):
+    print(index, letter)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
